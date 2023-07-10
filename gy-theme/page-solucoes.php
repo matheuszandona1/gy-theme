68fdc01d5dba66d886d3d6b1aa1d4a504620a965
diaslol <mastronelli88@gmail.com>
2023-07-10 15:11:10 -0300

    page solucoes

---
+++ gy-theme/page-solucoes.php
@@ -0,0 +1,23 @@
+<?php
+
+// Template Name: Soluções GY
+
+get_header();
+?>
+
+<main class="page--solucoes-gy">
+
+    <section class="solucoes">
+        <div class="solucoes__container">
+            <h2 class="default__title no-after">Soluções GreenYellow</h2>
+            <p class="default__desc">Soluções em energia inteligentes e integradas para transformar o seu ecossistema energético.</p>
+        </div>
+        <?php get_template_part('components/diagrama', 'solucoes'); ?>
+
+        <?php get_template_part('components/newsletter'); ?>
+    </section>
+
+</main>
+
+<?php
+get_footer();
